084e8f6e05bec6fb5256c2a6a912bf67af0aad5e
IGVDev <guidoverstraeten@gmail.com>
2024-06-11 13:23:22 -0300

    add cloudstorage for count

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import twaLogo from './assets/tapps.png'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import twaLogo from "./assets/tapps.png";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-import WebApp from '@twa-dev/sdk'
+import WebApp from "@twa-dev/sdk";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const initialCount = Number(WebApp.CloudStorage.getItem("count"));
+
+  const [count, setCount] = useState(initialCount);
+
+  const handleMainButtonClick = () => {
+    setCount((count) => count + 1);
+    WebApp.CloudStorage.setItem("count", count.toString());
+  };
 
   return (
     <>
@@ -24,18 +31,20 @@ function App() {
       </div>
       <h1>TWA + Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => handleMainButtonClick}>count is {count}</button>
       </div>
       {/*  */}
       <div className="card">
-        <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
-            Show Alert
+        <button
+          onClick={() =>
+            WebApp.showAlert(`Hello World! Current count is ${count}`)
+          }
+        >
+          Show Alert
         </button>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
