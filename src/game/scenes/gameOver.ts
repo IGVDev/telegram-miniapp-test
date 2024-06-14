84c78147e101d2ddb52c64e6bcda9b7fae93efcc
IGVDev <guidoverstraeten@gmail.com>
2024-06-14 15:26:11 -0300

    disable highscore

--- src/game/scenes/gameOver.ts
+++ src/game/scenes/gameOver.ts
@@ -4,19 +4,6 @@ export default class GameOverScene extends Phaser.Scene {
   }
 
   create() {
-    const highScore = localStorage.getItem("highScore") || "0";
-    this.add
-      .text(
-        this.scale.width / 2,
-        this.scale.height / 2 - 60,
-        `High Score: ${highScore}`,
-        {
-          fontSize: "32px",
-          color: "#fff",
-          align: "center",
-        }
-      )
-      .setOrigin(0.5);
     this.add
       .text(
         this.scale.width / 2,
