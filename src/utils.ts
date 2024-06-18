611817cb62ca826b35b82c1d8040e68bc9523a08
IGVDev <guidoverstraeten@gmail.com>
2024-06-18 13:39:18 -0300

    import cryptojs

--- src/utils.ts
+++ src/utils.ts
@@ -1,33 +1,34 @@
+import CryptoJS from "crypto-js";
+
 export const verifyTelegramWebAppData = async (
-    telegramInitData: string
-  ): Promise<boolean> => {
-    const initData = new URLSearchParams(telegramInitData);
-    const hash = initData.get("hash");
-    const dataToCheck: string[] = [];
+  telegramInitData: string
+): Promise<boolean> => {
+  const initData = new URLSearchParams(telegramInitData);
+  const hash = initData.get("hash");
+  const dataToCheck: string[] = [];
 
-    initData.sort();
-    initData.forEach(
-      (val, key) => key !== "hash" && dataToCheck.push(`${key}=${val}`)
-    );
+  initData.sort();
+  initData.forEach(
+    (val, key) => key !== "hash" && dataToCheck.push(`${key}=${val}`)
+  );
 
-    const secret = CryptoJS.HmacSHA256(
-      import.meta.env.VITE_BOT_TOKEN,
-      "WebAppData"
-    );
-    const _hash = CryptoJS.HmacSHA256(
-      dataToCheck.join("\n"),
-      secret
-    ).toString(CryptoJS.enc.Hex);
+  const secret = CryptoJS.HmacSHA256(
+    import.meta.env.VITE_BOT_TOKEN,
+    "WebAppData"
+  );
+  const _hash = CryptoJS.HmacSHA256(dataToCheck.join("\n"), secret).toString(
+    CryptoJS.enc.Hex
+  );
 
-    return _hash === hash;
-  };
+  return _hash === hash;
+};
 
 export const extractUserId = (initData) => {
-    if (!initData) return;
+  if (!initData) return;
 
-    const params = new URLSearchParams(initData);
-    const user = params.get("user");
-    if (!user) return;
-    const userData = JSON.parse(user);
-    return userData.id;
-  };
+  const params = new URLSearchParams(initData);
+  const user = params.get("user");
+  if (!user) return;
+  const userData = JSON.parse(user);
+  return userData.id;
+};
