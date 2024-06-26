02f2d739e938d1e90ce3b0ab056e7bfc26540c0a
IGVDev <guidoverstraeten@gmail.com>
2024-06-26 14:58:02 -0300

    implement log in

--- src/pages/tap.tsx
+++ src/pages/tap.tsx
@@ -4,52 +4,61 @@ import FlappyBirdGame from "../game/game";
 import coinImg from "../assets/coin.png";
 import WebApp from "@twa-dev/sdk";
 import { verifyTelegramWebAppData } from "../utils";
+import axios from "axios";
 // import axios from "axios";
 
 export const Tap = () => {
   const [coins, setCoins] = useState(0);
+  const [loggedIn, setLoggedIn] = useState(false);
 
   const handleScoreUpdate = (x: number) => {
     setCoins((prevCoins) => prevCoins + x);
   };
 
-  // useEffect(() => {
-  //   WebApp.CloudStorage.getItem("coins", (error, result) => {
-  //     if (error) {
-  //       console.error(error);
-  //     } else {
-  //       setCoins(Number(result));
-  //     }
-  //   });
-  // }, []);
-
-  // useEffect(() => {
-  //   WebApp.CloudStorage.setItem("coins", coins.toString(), (error, result) => {
-  //     if (error) {
-  //       console.error(error);
-  //     } else {
-  //       console.log(result);
-  //     }
-  //   });
-  // }, [coins]);
+  useEffect(() => {
+    WebApp.CloudStorage.getItem("coins", (error, result) => {
+      if (error) {
+        console.error(error);
+      } else {
+        setCoins(Number(result));
+      }
+    });
+  }, []);
 
   useEffect(() => {
+    WebApp.CloudStorage.setItem("coins", coins.toString(), (error, result) => {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log(result);
+      }
+    });
+  }, [coins]);
+
+  useEffect(() => {
+    if (loggedIn) return;
     const data = WebApp.initData;
 
     if (verifyTelegramWebAppData(data)) {
       const params = new URLSearchParams(data);
-      // const id = extractUserId(data);
       const hash = params.get("hash");
       const start_param = params.get("start_param");
-      // axios.post(
-      //   `https://europe-west6-stage-music-backend.cloudfunctions.net/memecoin_user_login`,
-      //   {
-      //     referrer_uid:
-      //   }
-      // );
-      console.log(start_param, hash);
+      axios.post(
+        `https://europe-west6-stage-music-backend.cloudfunctions.net/memecoin_user_login`,
+        start_param
+          ? {
+              referrer_uid: start_param,
+            }
+          : {},
+        {
+          headers: {
+            Authorization: `Bearer ${hash}`,
+          },
+        }
+      );
+      setLoggedIn(true);
     }
-  }, []);
+  }, [loggedIn]);
 
   return (
     <>
