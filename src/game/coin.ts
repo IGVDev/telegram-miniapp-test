bcd16bba3d47d1cab2ee9b673603da519a9f1270
IGVDev <guidoverstraeten@gmail.com>
2024-07-04 15:52:23 -0300

    performance

--- src/game/coin.ts
+++ src/game/coin.ts
@@ -2,7 +2,9 @@ export class Coin extends Phaser.Physics.Arcade.Sprite {
   constructor(scene: Phaser.Scene, x: number, y: number) {
     super(scene, x, y, 'coin');
     scene.add.existing(this);
-    scene.physics.add.existing(this, true);
+    scene.physics.add.existing(this);
+    this.setImmovable(true);
+    (this.body as Phaser.Physics.Arcade.Body).allowGravity = false;
   }
 
   reset(x: number, y: number): void {
--- src/game/pipe.ts
+++ src/game/pipe.ts
@@ -2,7 +2,9 @@ export class Pipe extends Phaser.Physics.Arcade.Sprite {
   constructor(scene: Phaser.Scene, x: number, y: number, texture: string, frame?: string | number) {
     super(scene, x, y, texture, frame);
     scene.add.existing(this);
-    scene.physics.add.existing(this, true);
+    scene.physics.add.existing(this);
+    this.setImmovable(true);
+    (this.body as Phaser.Physics.Arcade.Body).allowGravity = false;
   }
 
   reset(x: number, y: number, frame: number) {
