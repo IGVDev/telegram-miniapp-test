972b210baaaa0adb73b384a5caf563a9953a9950
IGVDev <guidoverstraeten@gmail.com>
2024-07-05 14:46:02 -0300

    test config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl';
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), basicSsl()],
   build: {
-    outDir: './docs'
+    outDir: './docs',
+    assetsInlineLimit: 0,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          phaser: ['phaser']
+        }
+      }
+    }
   },
   server: {
     host: '0.0.0.0',
     port: 5174
   },
-  base: 'https://igvdev.github.io/telegram-miniapp-test/'
-});
+  base: 'https://igvdev.github.io/telegram-miniapp-test/',
+  optimizeDeps: {
+    include: ['phaser'] 
+  },
+  resolve: {
+    alias: {
+      'phaser': 'phaser/dist/phaser.js' 
+    }
+  }
+})
