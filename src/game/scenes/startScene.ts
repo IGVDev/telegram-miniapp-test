4a61bff90bef47c95c99f16fac6f55804e88e5d8
IGVDev <guidoverstraeten@gmail.com>
2024-07-09 11:46:19 -0300

    change name

--- src/game/scenes/startScene.ts
+++ src/game/scenes/startScene.ts
@@ -4,8 +4,7 @@ export default class StartScene extends Phaser.Scene {
   }
 
   create() {
-    this.add.text(this.game.scale.width / 2, this.game.scale.height / 2 - 150, "FLY PATATA", { fontSize: 40, color: "#fff", align: "center" }).setOrigin(0.5);
-    this.add.text(this.game.scale.width / 2, this.game.scale.height / 2 - 100, "FLY AWAY!", { fontSize: 40, color: "#fff", align: "center" }).setOrigin(0.5);
+    this.add.text(this.game.scale.width / 2, this.game.scale.height / 2 - 100, "FLY PATATA FLY", { fontSize: 40, color: "#fff", align: "center" }).setOrigin(0.5);
     this.add.text(this.game.scale.width / 2, this.game.scale.height / 2, "Tap to start", { fontSize: 24, color: "#fff", align: "center" }).setOrigin(0.5);
 
     this.input.on("pointerdown", () => {
