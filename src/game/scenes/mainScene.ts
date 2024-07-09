ba58cf89a7d8e4de25aba33df1dd6c03f0713a20
IGVDev <guidoverstraeten@gmail.com>
2024-07-09 11:41:19 -0300

    change coin spawn rate

--- src/game/scenes/mainScene.ts
+++ src/game/scenes/mainScene.ts
@@ -593,7 +593,7 @@ export default class MainScene extends Phaser.Scene {
     this.pipeCounter++;
 
     const spawnChance = 0.25;
-    if (this.pipeCounter % 3 === 0 && Math.random() < spawnChance) {
+    if (Math.random() < spawnChance) {
       this.spawnCoin(pipeX, (hole + 1) * pipeHeight);
     }
   }
